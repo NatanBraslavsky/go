eaa320b89df8ab9dd87bac35bdc1432c9f473837
natan pereira braslavsky de oliveira <natanbraslavsky1@cbpf.br>
2026-03-12 14:51:55 -0300

    estudos go

--- cursos/curso_max/fundamentals/Structs/main.go
+++ cursos/curso_max/fundamentals/Structs/main.go
@@ -1,1 +1,33 @@
-package structs
+package main
+
+import (
+	"fmt"
+
+	"exemple.com/struct/user"
+)
+
+
+
+func main() {
+	firstName := getUserData("Digite o primeiro nome: ")
+	secoundName := getUserData("Digite seu sobrenome")
+	birthDate := getUserData("Digite sua data de aniversario: ")
+
+	var appUser *user.User
+
+	appUser, err := user.NewUser(firstName, secoundName, birthDate)
+	if err != nil {
+		fmt.Println("Error creating user:", err)
+		return
+	}
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
+}
+
+func getUserData(text string) string {
+	fmt.Println(text)
+	var value string
+	fmt.Scanln(&value)
+	return value
+} 
