25e7394e6996c63e6383e78d6b3805b15f45d8b7
NatanBraslavsky <natanbraslavsky1@gmail.com>
2026-03-11 22:09:48 -0300

    estudos golang

--- cursos/curso_max/Essential/main.go
+++ cursos/curso_max/Essential/main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
+
+
 func main() {
-	fmt.Println("Hello, World!")
+	revenue, expenses, taxRate := userInput()
+
+	profit, ratio := calculate(revenue, expenses, taxRate)
+
+	fmt.Printf("Profit: %.2f\n", profit)
+	fmt.Printf("Ratio: %.2f\n", ratio)
+}
+
+func userInput() (float64, float64, float64) {
+	var revenue, expenses, taxRate float64
+
+	fmt.Print("Enter total revenue: ")
+	fmt.Scanln(&revenue)
+
+	fmt.Print("Enter total expenses: ")
+	fmt.Scanln(&expenses)
+
+	fmt.Print("Enter tax rate (as a decimal): ")
+	fmt.Scanln(&taxRate)
+
+	return revenue, expenses, taxRate
+} 
+
+func calculate(revenue, expenses, taxRate float64) (float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return profit, ratio
 }
