ae3c6961c6ea309f142081ed7f0f0185f20907de
natan pereira braslavsky de oliveira <natanbraslavsky1@cbpf.br>
2026-03-16 09:44:14 -0300

    testes golang

---
+++ testes/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func pares(numbers []int) []int {
+	numPares := []int{}
+	for _, v := range numbers {
+		if v % 2 == 0 {
+			numPares = append(numPares, v)
+		}
+	}
+	return numPares
+}
+
+func main() {
+	numbers := []int{2,4,5,55,67,32}
+	numPares := pares(numbers)
+	fmt.Println(numPares)
+}
+
