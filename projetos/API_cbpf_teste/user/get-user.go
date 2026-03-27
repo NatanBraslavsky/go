cfc6cc7fbdb9b45a198984515281e49ec2b35460
natan pereira braslavsky de oliveira <natanbraslavsky1@cbpf.br>
2026-03-27 11:46:31 -0300

    fix: ajust error return

--- projetos/API_cbpf_teste/user/get-user.go
+++ projetos/API_cbpf_teste/user/get-user.go
@@ -2,24 +2,18 @@ package user
 
 import (
 	"context"
-	"errors"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
-	mongodriver "go.mongodb.org/mongo-driver/v2/mongo"
 
 	"exemplo.com/api-cbpf/models"
-	mongoconn "exemplo.com/api-cbpf/mongo"
+	"exemplo.com/api-cbpf/mongo"
 )
 
 func GetByID(ctx context.Context, id bson.ObjectID) (models.User, error) {
 	var foundUser models.User
 
-	err := mongoconn.ApiProject.Collection("user").FindOne(ctx, bson.M{"_id": id}).Decode(&foundUser)
+	err := mongo.ApiProject.Collection("user").FindOne(ctx, bson.M{"_id": id}).Decode(&foundUser)
 	if err != nil {
-		if errors.Is(err, mongodriver.ErrNoDocuments) {
-			return models.User{}, ErrUserNotFound
-		}
-
 		return models.User{}, err
 	}
 
